dfcd9b0061d7ca7b90a0acca29ea0e026bc064fb
Amira Sheremet <amira_sheremet@gmail.com>
2015-04-03 01:12:23 +0300

    EmailValidator to validate emails

---
+++ src/main/java/Utils/EmailValidator.java
@@ -0,0 +1,28 @@
+package Utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by amira on 03.04.15.
+ */
+public class EmailValidator {
+
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    public EmailValidator() {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+    }
+
+    public boolean validate(final String hex) {
+
+        matcher = pattern.matcher(hex);
+        return matcher.matches();
+
+    }
+}
